3b90685b1e2c0e419bec6fc580a454a533291d69
stephanetchatchum <stephanetchatchum@gmail.com>
2026-03-06 08:43:10 +0200

    Adding templates

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
+with open("data/twi.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+@app.route("/translate/<word>")
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return {"Word": word, "translation": data[word]}
+    else:
+        return {"Error": "Word not found"}
 @app.route("/")
 def home():
-    return "Afrilang is running"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
