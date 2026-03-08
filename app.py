6966cfd3d46d844c13e931bf78f77bb56c236de2
stephanetchatchum <stephanetchatchum@gmail.com>
2026-03-08 10:19:06 +0200

    Adding the list page

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def translate():
 def home():
     return render_template("index.html")
 
+@app.route("/list")
+def list():
+    return render_template("list.html", data=data)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
